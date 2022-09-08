6a803e0bdd397c15a01cd7d723ccdf332c2fc832
Choves-Nini <chovesnini@gmail.com>
2022-09-08 14:55:50 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio>
 
 /**
- * print out phrase in brackets
+ * Main - Entry point
  *
- * return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle"/n);
-return (0)
+	puts("/"Programming is like building a multilingual puzzle"/n);
+	return (0);
 }
