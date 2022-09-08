a12d93d7e70cd37576dad0ef8bf5ec45df927945
Choves-Nini <chovesnini@gmail.com>
2022-09-08 15:02:44 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * Main - Entry point
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle"/n);
+	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
 }
