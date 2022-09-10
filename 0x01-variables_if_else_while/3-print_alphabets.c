8da7c232b0864d1d2cf5999913f97ac63b1083fc
Choves-Nini <chovesnini@gmail.com>
2022-09-10 01:03:50 -0700

    upper and lowercase characters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ch;
+	int CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+
+	putchar(ch);
+
+	}
+	for (CH = 'A'; CH <= 'Z'; ch++)
+	{
+
+	putchar('CH');
+
+	}
+	{
+	putchar('\n');
+	}
+	
+	return (0);
+}
