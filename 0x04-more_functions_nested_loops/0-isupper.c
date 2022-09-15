4e6ad6bd4291913295dac3991fd40e26bb8c696c
Choves-Nini <chovesnini@gmail.com>
2022-09-15 06:15:23 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* 0-isupper.c - verifying uppercase numbers
+*
+* Return: 1 if positive and 0 otherwise
+*/
+
+int _isupper(int c);
+{
+	int c;
+
+i	for (c = 'A'; c <= 'Z'; c++)
+		return (1);
+	
+	else
+		return (0);
+}
