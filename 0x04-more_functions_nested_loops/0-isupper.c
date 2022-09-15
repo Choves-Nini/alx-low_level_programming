f1c54784be6acab26902fb1da9d6bc7a25f95f64
Choves-Nini <chovesnini@gmail.com>
2022-09-15 06:47:58 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,11 @@
 * Return: 1 if positive and 0 otherwise
 */
 
-int _isupper(int c);
+int _isupper(int c)
 {
-	int c;
-
-i	for (c = 'A'; c <= 'Z'; c++)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	
+
 	else
 		return (0);
 }
