32bf2539b2e939aec350bc806a122333801a147c
Choves-Nini <chovesnini@gmail.com>
2022-09-17 21:04:20 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,20 +6,26 @@
 
 void times_table(void)
 {
-	int num1, num2, prod;
+	int num, mult, prod;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
-			prod = num1 * num2
+			_putchar(',');
+			_putchar(' ');
 
-			if (num2 != 0)
-			{
-				_putchar(',');
+			prod = num * mult;
+
+			if (prod <= 9)
 				_putchar(' ');
-			}
+			else
+				_putchar((prod / 10) + '0');
 
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
-}
+
