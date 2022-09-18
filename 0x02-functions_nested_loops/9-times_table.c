329eab8333d567b68d4c7ba61b57141fe42b582d
Choves-Nini <chovesnini@gmail.com>
2022-09-17 20:28:01 -0700

    natural numbers

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,24 +6,20 @@
 
 void times_table(void)
 {
-	int num1;
-	int num2;
-	int prod;
-
-	num1 = 0;
-	num2 = 0;
-	prod = num1 * num2;
+	int num1, num2, prod;
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			_putchar(prod + '0');
-			_putchar(',');
-			_putchar(' ');
+			prod = num1 * num2
+
+			if (num2 != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 
 		}
-		_putchar('\n');
-
 	}
 }
