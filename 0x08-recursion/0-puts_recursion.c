ca4d9d15a2dd4a49a53e6b8f542c937d3a0e5012
Choves-Nini <chovesnini@gmail.com>
2022-09-28 06:20:50 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints out the contents of a string
+ * @s: string to be printed
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+		return;
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
+
