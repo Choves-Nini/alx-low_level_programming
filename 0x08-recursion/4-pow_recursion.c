a3a96f45a607629cd9efdbc585a69c413233ea75
Choves-Nini <chovesnini@gmail.com>
2022-09-28 22:33:24 -0700

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,6 +13,8 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-
-	return (x * (_pow_recursion(x, y - 1)));
+	if (y == 1)
+		return (x);
+	else
+		return (_pow_recursion(x, y - 1));
 }
