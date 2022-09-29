963442f7b53b6590dc2e9a870774e17b8b0c2204
Choves-Nini <chovesnini@gmail.com>
2022-09-28 22:21:26 -0700

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+  * _pow_recursion - function that raises x to the power y
+  * @x: the integer to be raised
+  * @y: the power to which the integer is raised
+  * Return: the value of x raised to y
+  */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 1)
